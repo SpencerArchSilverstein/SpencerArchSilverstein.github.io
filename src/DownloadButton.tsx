0768b6d92764b54ef4fe74ee3f2c84f9de3870b0
MusicTastingClubNU <musictastingclub@gmail.com>
2024-11-13 15:37:16 -0600

    Fixing bug with resume downloading

--- src/DownloadButton.tsx
+++ src/DownloadButton.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import DownloadIcon from "@mui/icons-material/Download";
+import pdfFile from "./ArchSilversteinResume.pdf";
+
 function DownloadButton() {
   return (
     <Button
-      href="./archsilverstein/src/SpencerArchSilversteinResume.pdf"
-      download="SpencerArchSilverstein.pdf"
+      href={pdfFile}
+      download="ArchSilversteinResume.pdf"
       startIcon={<DownloadIcon />}
+      style={{ width: 20, height: 50 }}
     ></Button>
   );
 }
